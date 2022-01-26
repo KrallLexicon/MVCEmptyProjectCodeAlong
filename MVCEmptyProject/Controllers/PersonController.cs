15e3f86d5c56fc8a873195a99cc6d79fdf6a2644
Jonathan Krall <Jonathan.Krall@lexicon.se>
2022-01-26 09:58:44 +0100

    Add project files.

---
+++ MVCEmptyProject/Controllers/PersonController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MVCEmptyProject.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVCEmptyProject.Controllers
+{
+    public class PersonController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult CheckPerson()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult CheckPerson(string name, int age)
+        {
+            PersonModel model = new PersonModel();
+
+            model.Age = age;
+            model.Name = name;
+
+            ViewBag.Message = PersonModel.Check(name, age);
+            return View();
+        }
+
+        public IActionResult SetSession()
+        {
+            HttpContext.Session.SetString("Test", "This is a test session");
+            return View();
+        }
+        public IActionResult GetSession()
+        {
+            ViewBag.Message = HttpContext.Session.GetString("Test");
+            return View(); 
+        }
+    }
+}
---
+++ MVCEmptyProject/Models/PersonModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVCEmptyProject.Models
+{
+    public class PersonModel
+    {
+        public static string Check(string name, int age)
+        {         
+
+            if (age >= 18)
+                return name + " is " + age + " years old and thus allowed to vote!";
+            else
+               return name + " is " + age + " years old and thus NOT allowed to vote!";           
+        }
+
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+}
